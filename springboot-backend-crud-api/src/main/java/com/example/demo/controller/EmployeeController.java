bdfcde417235304cf729c208fc6ca7e414edf88b
FachryRizano <fachry.rizano@gmail.com>
2020-11-15 17:58:43 +0700

    menambahkan get employee by id API di backend springboot

--- springboot-backend-crud-api/src/main/java/com/example/demo/controller/EmployeeController.java
+++ springboot-backend-crud-api/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +26,14 @@ public class EmployeeController {
     @PostMapping("employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    //get employee by id
+    @GetMapping("employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Employee not exist in id=" + id));
+        return ResponseEntity.ok(employee);
     }
 
 }
